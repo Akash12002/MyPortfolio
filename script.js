cc6242fc1ff3236b0e3a2aacb4f33796e0fca888
Akash sunil Zanjari <121276369+Akash12002@users.noreply.github.com>
2023-05-18 21:12:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer','Aspirant Engineer','Android Developer',''],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
